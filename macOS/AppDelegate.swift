4316681deb196d0e4d00f194584dedf2358a7d6e
Albertino Padin <albertino.padin@upr.edu>
2017-04-19 00:37:16 -0700

    Initial Commit

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Conway's Game of Life
+//
+//  Created by Albertino Padin on 4/19/17.
+//  Copyright © 2017 Albertino Padin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
